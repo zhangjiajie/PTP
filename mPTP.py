7bc65843a38d564ee21c2bf869a0f3ddcdda63ef
zhangjiajie <bestzhangjiajie@gmail.com>
2013-11-28 11:55:20 +0100

    fix bugs for single outgroup

--- mPTP.py
+++ mPTP.py
@@ -186,33 +186,47 @@ class mptp:
 		self.numtaxa = len(self.taxa_order)
 		self.numtrees = len(self.trees)
 	
-	def remove_outgroups(self, ognames):
-		for og in ognames:
-			self.taxa_order.remove(og)
-			self.numtaxa = len(self.taxa_order)
-		for i in range(len(self.trees)):
-			t = Tree(self.trees[i])
-			ancestor = t.get_common_ancestor(ognames)
-			if not t == ancestor:
-				t.set_outgroup(ancestor)
-			t.prune(self.taxa_order, preserve_branch_length=True)
-			t.show()
-			self.trees[i] = t.write()
+	def remove_outgroups(self, ognames, remove = False):
+		try:
+			if remove:
+				for og in ognames:
+					self.taxa_order.remove(og)
+				self.numtaxa = len(self.taxa_order)
+			for i in range(len(self.trees)):
+				t = Tree(self.trees[i])
+				if len(ognames) < 2:
+					t.set_outgroup(ognames[0])
+					if remove:
+						t.prune(self.taxa_order, preserve_branch_length=True)
+				else:
+					ancestor = t.get_common_ancestor(ognames)
+					if not t == ancestor:
+						t.set_outgroup(ancestor)
+					if remove:
+						t.prune(self.taxa_order, preserve_branch_length=True)
+				self.trees[i] = t.write()
+		except ValueError, e:
+			print(e)
+			print("")
+			print("")
+			print("Somthing is wrong with the input outgroup names")
+			print("")
+			print("Quiting .....")
+			sys.exit()
 	
 	def delimit(self, fout, sreroot = False, pvalue = 0.001, weight = 1):
 		self.weight = weight
-		#output = open(fout, "a")
-		#output.write("#"+self.print_list(self.taxa_order))
 		self.partitions = []
+		cnt = 1 
 		for tree in self.trees:
+			print("Delimiting on tree " + repr(cnt) + "........")
+			cnt = cnt + 1
 			me = exponential_mixture(tree= tree, max_iters = 20000, min_br = 0.0001 )
 			me.search(reroot = sreroot, strategy = "H0")
 			me.count_species(pv = pvalue)
 			order, partition = me.output_species(self.taxa_order)
 			self.partitions.append(partition)
-			#output.write(str(weight) + ":" + self.print_list(partition))
-			
-		#output.close()
+			print("")
 		besttreeidx = self.summary(fout)
 		return self.partitions
 	
@@ -324,6 +338,7 @@ def print_options():
 		print("    -o output_name                 Specify output file name.\n")
 		print("    -g outgroupnames               t1,t2,t3  commma delimt and no space in between")
 		print("    -r                             Rooting the input tree on the longest branch.(default not)\n")
+		print("    -d                             Remove outgroups specified by -g. (default not)\n")
 		print("    -pvalue (0-1)                  Set the p-value for likelihood ratio test.(default 0.001)")
 
 
@@ -332,8 +347,6 @@ if __name__ == "__main__":
 	print("Version 0.1 released by Jiajie Zhang on 25-11-2013\n")
 	print("This program will delimit species on multiple phylogenetic trees.")
 	print("The input file should be in NEXUS format (such as the output from MrBayes).")
-	#print("PTP can also infer phylogenetic tree using RAxML, currently only support DNA on GTRGAMMA.\n")
-	#print("The program needs ETE(http://ete.cgenomics.org/) package to be installed.\n")
 	print("--Please cite: \"J. Zhang, P. Kapli, P. Pavlidis, A. Stamatakis: A General") 
 	print("--Species Delimitation Method with Applications to Phylogenetic Placements. ")
 	print("--Bioinformatics (2013), 29 (22): 2869-2876.\" ")
@@ -345,56 +358,28 @@ if __name__ == "__main__":
 		print_options()
 		sys.exit()
 	
-	salignment = ""
 	stree = ""
 	sreroot = False
-	sstrategy = "H0"
-	sprint = False 
-	sshow = False
-	sscale = 500
-	max_iter = 20000
-	min_brl = 0.0001
-	spe_rate = -1.0
-	whiten = False
 	pvalue = 0.001
 	ptpout = ""
 	outgroups = []
+	rmog = False
 	
 	for i in range(len(sys.argv)):
 		if sys.argv[i] == "-t":
 			i = i + 1
 			stree = sys.argv[i]
-		elif sys.argv[i] == "-m":
-			i = i + 1
-			sstrategy = sys.argv[i]
 		elif sys.argv[i] == "-r":
 			sreroot = True
-		elif sys.argv[i] == "-s":
-			sshow = True
+		elif sys.argv[i] == "-d":
+			rmog = True
 		elif sys.argv[i] == "-g":
 			i = i + 1
 			ogs = sys.argv[i].strip()
 			outgroups = ogs.split(",")
-			
-		elif sys.argv[i] == "-p":
-			sprint = True 
-		elif sys.argv[i] == "-maxiters":
-			i = i + 1
-			max_iter = int(sys.argv[i])
-		elif sys.argv[i] == "-minbr":
-			i = i + 1
-			min_brl = float(sys.argv[i])
 		elif sys.argv[i] == "-pvalue":
 			i = i + 1
 			pvalue = float(sys.argv[i])
-		elif sys.argv[i] == "-sprate":
-			i = i + 1
-			spe_rate = float(sys.argv[i])
-		elif sys.argv[i] == "-w":
-			whiten = True
-		elif sys.argv[i] == "-a":
-			i = i + 1
-			salignment = sys.argv[i]
 		elif sys.argv[i] == "-o":
 			i = i + 1
 			ptpout = sys.argv[i]
@@ -405,15 +390,14 @@ if __name__ == "__main__":
 			print_options()
 			sys.exit()
 	
-	
 	if stree == "":
 		print("The input tree is empty.")
 		print_options()
 		sys.exit()
-		
+	
 	if ptpout == "":
 		ptpout = stree + ".out"
-
+	
 	try:
 		treetest = open(stree)
 		l1 = treetest.readline()
@@ -423,13 +407,10 @@ if __name__ == "__main__":
 			inputformat = "nexus"
 		else:
 			inputformat = "raxml"
-		
 		mp = mptp(filename = stree, ftype = inputformat)
 		if len(outgroups) > 0:
-			mp.remove_outgroups(outgroups)
+			mp.remove_outgroups(outgroups, remove = rmog)
 		mp.delimit(fout = ptpout, sreroot = sreroot, pvalue = pvalue, weight = 1)
-		
-		
 	except ete2.parser.newick.NewickError:
 		print("Unexisting tree file or Malformed newick tree structure.")
 
